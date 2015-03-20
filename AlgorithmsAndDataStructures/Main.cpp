905699b76d8e6f21f90667064d27c643ad77d02f
Nicholas Judd <nicholas_o_judd@hotmail.com>
2015-03-20 13:37:55 +1100

    Update Main.cpp

--- AlgorithmsAndDataStructures/Main.cpp
+++ AlgorithmsAndDataStructures/Main.cpp
@@ -2,10 +2,6 @@
 
 int main(int argc, const char* argv[]){
     //Application entry point
-    for (int i = 0; i < 1; i++){
-        std::cout << "Isint this a nice lil test" << std::endl;
-        Std::endl << "BOOB" << std::endl;
-    }
     MainLoop::Start();
     return 0;
 }

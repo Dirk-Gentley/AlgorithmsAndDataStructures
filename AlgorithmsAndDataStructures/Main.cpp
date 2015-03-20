f382cc48b8c861cecf72606cbe61b29096f4ea20
NIcholasJudd <noj999@uowmail.edu.au>
2015-03-20 12:52:40 +1100

    TEsy
    this is a lovely lil test

--- AlgorithmsAndDataStructures/Main.cpp
+++ AlgorithmsAndDataStructures/Main.cpp
@@ -2,6 +2,10 @@
 
 int main(int argc, const char* argv[]){
     //Application entry point
+    for (int i = 0; i < 1; i++){
+        std::cout << "Isint this a nice lil test" << std::endl;
+        Std::endl << "BOOB" << std::endl;
+    }
     MainLoop::Start();
     return 0;
 }

3a8f5ae2ab78c35add2a65bc34a3f8d7f710db64
Nicholas Judd <noj999@uowmail.edu.au>
2015-04-17 08:47:02 +1000

    update wordpress

--- Website/minutes.js
+++ Website/minutes.js
@@ -13,6 +13,38 @@ angular.module('ui.bootstrap.demo').controller('minuets', function ($scope) {
     {
       title: 'Dynamic Group Header - 2',
       content: 'meeting-minutes-06-03-2015.pdf'
+    },
+    {
+      title: 'Dynamic Group Header - 1',
+      content: 'meeting-minutes-02-03-2015.pdf'
+    },
+    {
+      title: 'Dynamic Group Header - 2',
+      content: 'meeting-minutes-06-03-2015.pdf'
+    },
+    {
+      title: 'Dynamic Group Header - 1',
+      content: 'meeting-minutes-02-03-2015.pdf'
+    },
+    {
+      title: 'Dynamic Group Header - 2',
+      content: 'meeting-minutes-06-03-2015.pdf'
+    },
+    {
+      title: 'Dynamic Group Header - 1',
+      content: 'meeting-minutes-02-03-2015.pdf'
+    },
+    {
+      title: 'Dynamic Group Header - 2',
+      content: 'meeting-minutes-06-03-2015.pdf'
+    },
+    {
+      title: 'Dynamic Group Header - 1',
+      content: 'meeting-minutes-02-03-2015.pdf'
+    },
+    {
+      title: 'Dynamic Group Header - 2',
+      content: 'meeting-minutes-06-03-2015.pdf'
     }
   ];
   });

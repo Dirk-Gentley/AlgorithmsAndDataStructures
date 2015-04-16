5b2e4015b5451f27e2940cc00a3222211b0e3691
Nicholas Judd <noj999@uowmail.edu.au>
2015-04-17 08:30:37 +1000

    cause i said so

---
+++ Website/minutes.js
@@ -0,0 +1,18 @@
+angular.module('ui.bootstrap.demo', ['ui.bootstrap'])
+.constant('minuetsConfig', {
+        fullHeight: true
+    })
+angular.module('ui.bootstrap.demo').controller('minuets', function ($scope) {
+  $scope.oneAtATime = true;
+
+  $scope.groups = [
+    {
+      title: 'Dynamic Group Header - 1',
+      content: 'meeting-minutes-02-03-2015.pdf'
+    },
+    {
+      title: 'Dynamic Group Header - 2',
+      content: 'meeting-minutes-06-03-2015.pdf'
+    }
+  ];
+  });

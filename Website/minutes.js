b34e4cdcc4510d7b2f9ca2b5822818458f63efa6
Nicholas Judd <noj999@uowmail.edu.au>
2015-04-17 14:49:31 +1000

    Cause i Can

--- Website/minutes.js
+++
@@ -1,7 +0,0 @@
-angular.module('ui.bootstrap.demo', ['ui.bootstrap']);
-angular.module('ui.bootstrap.demo').controller('TabsDemoCtrl', function ($scope, $window) {
-  $scope.tabs = [
-    { title:'Group Meeting 1', content:'meeting-minutes-02-03-2015.pdf' },
-    { title:'Group Meeting 2', content:'meeting-minutes-06-03-2015.pdf'}
-  ];
-});

cf35c1f379d4f6f38aff8936ec9fbb5a257aadde
Nicholas Judd <noj999@uowmail.edu.au>
2015-04-17 10:12:03 +1000

    hacky shit

--- Website/minutes.js
+++ Website/minutes.js
@@ -1,50 +1,7 @@
-angular.module('ui.bootstrap.demo', ['ui.bootstrap'])
-.constant('minuetsConfig', {
-        fullHeight: true
-    })
-angular.module('ui.bootstrap.demo').controller('minuets', function ($scope) {
-  $scope.oneAtATime = true;
-
-  $scope.groups = [
-    {
-      title: 'Dynamic Group Header - 1',
-      content: 'meeting-minutes-02-03-2015.pdf'
-    },
-    {
-      title: 'Dynamic Group Header - 2',
-      content: 'meeting-minutes-06-03-2015.pdf'
-    },
-    {
-      title: 'Dynamic Group Header - 1',
-      content: 'meeting-minutes-02-03-2015.pdf'
-    },
-    {
-      title: 'Dynamic Group Header - 2',
-      content: 'meeting-minutes-06-03-2015.pdf'
-    },
-    {
-      title: 'Dynamic Group Header - 1',
-      content: 'meeting-minutes-02-03-2015.pdf'
-    },
-    {
-      title: 'Dynamic Group Header - 2',
-      content: 'meeting-minutes-06-03-2015.pdf'
-    },
-    {
-      title: 'Dynamic Group Header - 1',
-      content: 'meeting-minutes-02-03-2015.pdf'
-    },
-    {
-      title: 'Dynamic Group Header - 2',
-      content: 'meeting-minutes-06-03-2015.pdf'
-    },
-    {
-      title: 'Dynamic Group Header - 1',
-      content: 'meeting-minutes-02-03-2015.pdf'
-    },
-    {
-      title: 'Dynamic Group Header - 2',
-      content: 'meeting-minutes-06-03-2015.pdf'
-    }
+angular.module('ui.bootstrap.demo', ['ui.bootstrap']);
+angular.module('ui.bootstrap.demo').controller('TabsDemoCtrl', function ($scope, $window) {
+  $scope.tabs = [
+    { title:'Dynamic Title 1', content:'meeting-minutes-02-03-2015.pdf' },
+    { title:'Dynamic Title 2', content:'meeting-minutes-06-03-2015.pdf'}
   ];
-  });
+});

ef137b0f6dfb8724ac5cb176e2827d7baeb26641
Nicholas Judd <noj999@uowmail.edu.au>
2015-05-04 08:31:20 +1000

    website

--- Website/docs.js
+++ Website/docs.js
@@ -1,7 +1,7 @@
 angular.module('ui.bootstrap.demo', ['ui.bootstrap']);
 angular.module('ui.bootstrap.demo').controller('TabsDemoCtrl', function ($scope, $window) {
   $scope.tabs = [
-    { title:'Coding Style Guide', content:'Coding-Style.pdf' },
-    { title:'Requirements and Specifications', content:'Requirements-and-Specifications.pdf'}
+    { title:'Coding Style Guide', content:'Documents/Coding-Style.pdf' },
+    { title:'Requirements and Specifications', content:'Documents/Requirements-and-Specifications.pdf'}
   ];
 });

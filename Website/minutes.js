e3e0ba225f434caae85301eff3fdf2a424b46be6
Nicholas Judd <noj999@uowmail.edu.au>
2015-05-11 10:57:03 +1000

    website

--- Website/minutes.js
+++ Website/minutes.js
@@ -8,6 +8,6 @@ angular.module('ui.bootstrap.demo').controller('TabsDemoCtrl', function ($scope,
     { title:'Group Meeting 5', content:'minutes/27-03-15.pdf' },
     { title:'Group Meeting 6', content:'minutes/10-04-2015.pdf'},
     { title:'Group Meeting 7', content:'minutes/17-04-15.pdf'},
-    ( title:'Group Meeting 8', content:'minutes/27-04-15.pdf'}
+    { title:'Group Meeting 8', content:'minutes/27-04-15.pdf'}
   ];
 });

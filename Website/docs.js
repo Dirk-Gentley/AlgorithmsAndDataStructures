9bfa155bf735f4611fb2d6ddfb0aed8223d90e1d
Nicholas Judd <nicholasjudd@Nicholass-MacBook-Pro.local>
2015-11-04 10:46:00 +1100

    web

--- Website/docs.js
+++ Website/docs.js
@@ -1,7 +1,8 @@
 angular.module('ui.bootstrap.demo', ['ui.bootstrap']);
 angular.module('ui.bootstrap.demo').controller('TabsDemoCtrl', function ($scope, $window) {
   $scope.tabs = [
-    { title:'Coding Style Guide', content:'Documents/Coding-Style.pdf' },
-    { title:'Requirements and Specifications', content:'Documents/Requirements-and-Specifications.pdf'}
+    { title:'Coding Style Guide', content:'Documents/Coding-StyleV2.pdf' },
+    { title:'Requirements and Specifications', content:'Documents/Requirements-Specifications.pdf'},
+    { title:'Programmers Guide', content:'Documents/Programmers-Guide.pdf'}
   ];
 });
